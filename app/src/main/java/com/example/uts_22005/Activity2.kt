5103b87d8b77a64296b3c34cda956766a313dd98
mozza-p <pramozza@gmail.com>
2024-05-17 11:29:39 +0700

    update project uts

--- app/src/main/java/com/example/uts_22005/Activity2.kt
+++ app/src/main/java/com/example/uts_22005/Activity2.kt
@@ -1,12 +1,19 @@
 package com.example.uts_22005
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Activity2 : AppCompatActivity() {
+    companion object {
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_BATASUSIA = "extra_batasusia"
+        const val EXTRA_DURATION= "extra_duration"
+        const val EXTRA_YEAR= "extra_year"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,21 @@ class Activity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val textReceivedTitle: TextView = findViewById(R.id.receivedTitle)
+        val textReceivedBatasUsia: TextView = findViewById(R.id.receivedBatasUsia)
+        val textReceivedDuration: TextView = findViewById(R.id.receivedDuration)
+        val textReceivedYear: TextView = findViewById(R.id.receivedYear)
+
+        //simpan data String dari kiriman intent ke dalam variabel
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val batasusia = intent.getStringExtra(EXTRA_BATASUSIA)
+        val durasi = intent.getStringExtra(EXTRA_DURATION)
+        val year = intent.getStringExtra(EXTRA_YEAR)
+
+        //mengganti teks pada TextView berdasarkan data yang dikirim dari activity 2
+        textReceivedTitle.text = "Title: $title"
+        textReceivedBatasUsia.text = "Watch for: $batasusia"
+        textReceivedBatasUsia.text = "Watch for: $batasusia"
+        textReceivedDuration.text = "Duration: $durasi"
     }
 }
--- app/src/main/java/com/example/uts_22005/MainActivity.kt
+++ app/src/main/java/com/example/uts_22005/MainActivity.kt
@@ -12,50 +12,60 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uts_22005.Activity2
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var edTitle: EditText
     lateinit var btn:Button
     lateinit var teksBatasUsia : String
+    lateinit var teksYear : String
     lateinit var teksGenre: String
+    lateinit var teksDuration: String
     lateinit var radiobuttonChecked: RadioButton
     lateinit var adult: RadioButton
     lateinit var kids: RadioButton
     lateinit var radioGroup: RadioGroup
-    lateinit var checkBoxChecked: CheckBox
-    lateinit var
-    lateinit var spinnerMenu: Spinner
+    lateinit var checkBoxThriller: CheckBox
+    lateinit var checkBoxRomance: CheckBox
+    lateinit var checkBoxHorror: CheckBox
+    lateinit var checkBoxComedy: CheckBox
+    lateinit var checkBoxAction: CheckBox
+    lateinit var checkBoxFantasy: CheckBox
+    lateinit var spinnerYear: Spinner
+    lateinit var spinnerDurHour: Spinner
+    lateinit var spinnerDurMinute: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        edNama = findViewById(R.id.namaMenu)
+        edTitle = findViewById(R.id.edtextTitle)
         btn = findViewById(R.id.btn)
-        dineIn = findViewById(R.id.dineIn)
-        takeAway = findViewById(R.id.takeAway)
-        spinnerMenu = findViewById(R.id.spinnerMenu)
-        radioGroup = findViewById(R.id.bgroup)
+        adult = findViewById(R.id.rdButtonAdult)
+        kids = findViewById(R.id.rdButtonKids)
+        radioGroup = findViewById(R.id.rdGroupBatasUsia)
+        spinnerYear = findViewById(R.id.spinnerYear)
+        spinnerDurHour = findViewById(R.id.spinnerHours)
+        spinnerDurMinute = findViewById(R.id.spinnerMinutes)
 
         btn.setOnClickListener {
-            //ambil teks dari RadioButton berdasarkan lingkup RadioGroup
-            val selectedID = radioGroup!!.checkedRadioButtonId
-            radiobuttonChecked = findViewById(selectedID)
-            teksLayanan = radiobuttonChecked.text.toString()
+            val selectedUsia = radioGroup!!.checkedRadioButtonId
+            radiobuttonChecked = findViewById(selectedUsia)
+            teksBatasUsia = radiobuttonChecked.text.toString()
+            teksDuration = spinnerDurHour.selectedItem.toString()
+            teksYear = spinnerYear.selectedItem.toString()
 
-            //ambil teks dari Spinner
-            teksMenu = spinnerMenu.selectedItem.toString()
-
-            val moveIntent = Intent(this@MainActivity2, MainActivity3::class.java)
-            moveIntent.putExtra(MainActivity3.EXTRA_NAME,  edNama.text.toString())
-            moveIntent.putExtra(MainActivity3.EXTRA_LAYANAN, teksLayanan)
-            moveIntent.putExtra(MainActivity3.EXTRA_MENU, teksMenu)
+            val moveIntent = Intent(this@MainActivity, Activity2::class.java)
+            moveIntent.putExtra(Activity2.EXTRA_TITLE,  edTitle.text.toString())
+            moveIntent.putExtra(Activity2.EXTRA_BATASUSIA, teksBatasUsia)
+            moveIntent.putExtra(Activity2.EXTRA_DURATION, teksDuration)
+            moveIntent.putExtra(Activity2.EXTRA_YEAR, teksYear)
             startActivity(moveIntent)
         }
 
